ac8ca9bd7b271349694e92442fcd73feea8a47c7
gustavo-exe <gus.elim2@gmail.com>
2021-05-14 21:05:32 -0600

    Redaccion de acerca de mi, en la tarejta del proyecto LoginApp y limpieza de codigo.

--- javascript/RemoverMenuDesplegable.js
+++ javascript/RemoverMenuDesplegable.js
@@ -3,6 +3,5 @@ const Menu = document.getElementById('MenuDesplegable');
 
 const RemoverMenu = () =>{
     Menu.classList.remove('Active')
-    console.log('click')
 }
 Enlace.addEventListener('click',RemoverMenu);
