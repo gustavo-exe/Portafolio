be1d1fc3d1bcd9735f3b09d860092ff9cf88c819
gustavo-exe <gus.elim2@gmail.com>
2021-05-14 20:08:59 -0600

    Eliminando funcion para mostrar la descripcion porque funciona bien solo el hover para un dispositivo touch.

--- javascript/Descripcion.js
+++
@@ -1,8 +0,0 @@
-const Item = document.getElementById('Item');
-const Descripcion = document.getElementById('Item');
-
-const DesplegarDescripcion = () =>{
-    Descripcion.classList.toggle('Active')
-    console.log('click')
-}
-Item.addEventListener('click',DesplegarDescripcion);
