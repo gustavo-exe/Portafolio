1f505ae061fbbcd886dc142f76ce5a1b3aa72b9e
gustavo-exe <gus.elim2@gmail.com>
2021-08-12 17:46:56 -0600

    Icono para indicar al usuario que cierre el menu que se encuentra en la izquierda.

--- javascript/HamburgerMenu.js
+++ javascript/HamburgerMenu.js
@@ -1,7 +1,13 @@
 const button = document.getElementById('Despliegue');
 const menu = document.getElementById('MenuDesplegable');
-
+var toggle = false;
 const DesplegarMenu = () =>{
-    menu.classList.toggle('Active')
+    menu.classList.toggle('Active');
+    if (toggle) {
+        document.getElementById("Icono_De_Menu").src = '../imgs/icons/menu_black_24dp.svg';
+    }else{
+        document.getElementById("Icono_De_Menu").src = '../imgs/icons/icons8-eliminar-100.png';
+    }
+    toggle = !toggle; 
 }
 button.addEventListener('click',DesplegarMenu);
