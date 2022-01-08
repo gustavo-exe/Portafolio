3b92e158c5a8007a0db237ca22a0feacb9051f37
Gustavo Meza <61214852+gustavo-exe@users.noreply.github.com>
2022-01-07 20:06:27 -0600

    Iconos del menu

--- javascript/RemoverMenuDesplegable.js
+++ javascript/RemoverMenuDesplegable.js
@@ -1,7 +1,7 @@
-const Enlace = document.getElementById('EnlaceMenuDesplegable');
 const Menu = document.getElementById('MenuDesplegable');
 
 const RemoverMenu = () =>{
     Menu.classList.remove('Active')
+    document.getElementById("Icono_De_Menu").src = 'https://raw.githubusercontent.com/gustavo-exe/Portafolio/955d16f64eabd635556d5a55d2a7a503b2069e82/imgs/icons/menu_black_24dp.svg';
 }
 Enlace.addEventListener('click',RemoverMenu);
