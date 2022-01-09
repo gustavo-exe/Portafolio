e64026a42e93f02f03892696167fe542b1110ec7
Gustavo Meza <61214852+gustavo-exe@users.noreply.github.com>
2022-01-08 20:57:56 -0600

    Nueva informacion, optimizacion y correcion de advertencias

--- javascript/RemoverMenuDesplegable.js
+++ javascript/RemoverMenuDesplegable.js
@@ -4,4 +4,3 @@ const RemoverMenu = () =>{
     Menu.classList.remove('Active')
     document.getElementById("Icono_De_Menu").src = 'https://raw.githubusercontent.com/gustavo-exe/Portafolio/955d16f64eabd635556d5a55d2a7a503b2069e82/imgs/icons/menu_black_24dp.svg';
 }
-Enlace.addEventListener('click',RemoverMenu);
