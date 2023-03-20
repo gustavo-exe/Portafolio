f49f262d7bb42640f3834a4c9d9fb159dfe4c8aa
gustavo-exe <gustavo.meza@cit.hn>
2023-03-20 12:53:31 -0600

    Navigation var

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
 
+import styles from './NavBar.module.scss';
 
 export const NavBar = () => {
 
-return( 
+    return (
 
-    <header >
-        <div   >
-            <div ></div>
+        <div className="container-layout border-b items-center border-b-black flex justify-between pt-2 pb-2" >
+            <div className='flex  items-center '   >
+                <div className={styles.logo} ></div>
+                <div   >
+                    gustavoeliasexe
+                </div>
+
+            </div>
+
             <div  >
-                console.log("elias.js")
+                <nav className="flex gap-2" >
+                    <a href="#AcercaDe" >Acerca de Elias</a>
+                    <a href="#Actividades" >Actividades</a>
+                    <a href="#CosasQuePuedoHacer" >I can do</a>
+                    <a href="#Herramientas" >Herramientas</a>
+                    <a href="#Proyectos" >Proyectos</a>
+                </nav>
             </div>
         </div>
 
-        <div  >
-            <button id="Despliegue"  >
-                <img id="Icono_De_Menu" src="./imgs/icons/menu_black_24dp.svg" alt="Icono de hamburgesa" ></img>
-            </button>
-            <nav  >
-                <a href="#AcercaDe" >Acerca de Elias</a>
-                <a href="#Actividades" >Actividades</a>
-                <a href="#CosasQuePuedoHacer" >I can do</a>
-                <a href="#Herramientas" >Herramientas</a>
-                <a href="#Proyectos" >Proyectos</a>
-            </nav>
-        </div>
-    </header>
-
     )
 }
+;
