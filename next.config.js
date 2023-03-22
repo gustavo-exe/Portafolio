8829fd8e33c3bd49254220990ab4585efa88f15f
gustavo-exe <gustavo.meza@cit.hn>
2023-03-22 14:17:13 -0600

    page extension

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const path = require('path')
 
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   }
