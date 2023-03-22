d7eb05c2d22c2dec9cd40055b9b6205b24ab3f69
gustavo-exe <gustavo.meza@cit.hn>
2023-03-22 13:49:44 -0600

    Icon, lenguage and tittle

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="es">
       <Head >
       <script src="https://cdn.tailwindcss.com"></script>
+       <title>Gustavo Elias</title>
       </Head>
       <body>
         <Main />
