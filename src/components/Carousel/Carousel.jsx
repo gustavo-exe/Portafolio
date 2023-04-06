176e24a0f95c69598e2e715d97d11c759e853ee4
gustavo-exe <gustavo.meza@cit.hn>
2023-04-06 13:50:48 -0600

    Carousel

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react"
+
+export default function Carousel({
+  children: slides,
+  autoSlide = false,
+  autoSlideInterval = 3000,
+}) {
+  const [curr, setCurr] = useState(0)
+
+  const prev = () =>
+    setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1))
+  const next = () =>
+    setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
+
+  useEffect(() => {
+    if (!autoSlide) return
+    const slideInterval = setInterval(next, autoSlideInterval)
+    return () => clearInterval(slideInterval)
+  }, [])
+  return (
+    <div className="overflow-hidden relative">
+      <div
+        className="flex transition-transform ease-out duration-500"
+        style={{ transform: `translateX(-${curr * 100}%)` }}
+      >
+        {slides}
+      </div>
+      <div className="absolute inset-0 flex items-center justify-between p-4">
+              <button
+                  onClick={prev}
+                  className="p-1"
+              >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#333533" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                  </svg>
+              </button>
+
+              <button
+                  onClick={next}
+                  className="p-1"
+              >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#333533" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                  </svg>
+              </button>
+      </div>
+
+      <div className=" mt-4 bottom-4 right-0 left-0">
+        <div className="flex items-center justify-center gap-2">
+          {slides.map((_, i) => (
+            <div
+              className={`
+              transition-all w-2 h-2  rounded-full
+              ${curr === i ? " bg-dark-yet" : "bg-opacity-50 bg-variant"}
+            `}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/sections/portafolio/Proyects/Proyects.tsx
+++ src/sections/portafolio/Proyects/Proyects.tsx
@@ -1,6 +1,8 @@
 
 import { ProyectGallery } from "./Proyect/ProyectGallery/ProyectGallery";
 import { ProyectHeader } from "./Proyect/ProyectHeader/ProyectHeader";
+import style from "./Proyects.module.scss";
+import  Carousel  from "../../../components/Carousel/Carousel";
 
 export const Proyects = () => {
 
@@ -8,6 +10,71 @@ export const Proyects = () => {
     const listProyects = [
         {
             id: 0,
+            header: {
+                
+                    nombre: 'Pokemon x AR',
+                    logo: 'https://user-images.githubusercontent.com/61214852/118196015-8391ae00-b409-11eb-9cbc-a40b9bae3aa5.png',
+                    descripcion: `
+                        Es una pagina que cree con los diferentes tipos de realidad aumentada en AR.js con un plus de Model Viewer.
+                        Logre obtener los conocimientos para realizar el proyecto en EDteam en el curso Realida aumentada en la web con AR.js.
+                        Tengo pensado ir modificando o adaptando el sitio web, para explotar mejor los conocimientos.
+                    `,
+                    publictSite: 'https://gustavo-exe.github.io/ArJsPokemon/',
+                    repositorio: 'https://github.com/gustavo-exe/ArJsPokemon',
+                    tools: ['2', '4' , '11', '17']
+                
+            },
+            gallery: [
+                'https://raw.githubusercontent.com/gustavo-exe/Portafolio/c0ff4151bc7d4597166e944ba537205f37890d1f/imgs/MockupPokemon.jpg',
+                'https://raw.githubusercontent.com/gustavo-exe/ArJsPokemon/master/assets/Portadas/ImageTracking.jpg'
+            ]
+        },
+        {
+            id: 1,
+            header: {
+                
+                    nombre: 'Pokemon x AR',
+                    logo: 'https://user-images.githubusercontent.com/61214852/118196015-8391ae00-b409-11eb-9cbc-a40b9bae3aa5.png',
+                    descripcion: `
+                        Es una pagina que cree con los diferentes tipos de realidad aumentada en AR.js con un plus de Model Viewer.
+                        Logre obtener los conocimientos para realizar el proyecto en EDteam en el curso Realida aumentada en la web con AR.js.
+                        Tengo pensado ir modificando o adaptando el sitio web, para explotar mejor los conocimientos.
+                    `,
+                    publictSite: 'https://gustavo-exe.github.io/ArJsPokemon/',
+                    repositorio: 'https://github.com/gustavo-exe/ArJsPokemon',
+                    tools: ['2', '4' , '11', '17']
+                
+            },
+            gallery: [
+                'https://raw.githubusercontent.com/gustavo-exe/Portafolio/c0ff4151bc7d4597166e944ba537205f37890d1f/imgs/MockupPokemon.jpg',
+                'https://raw.githubusercontent.com/gustavo-exe/ArJsPokemon/master/assets/Portadas/ImageTracking.jpg'
+            ]
+        },
+        
+        {
+            id: 2,
+            header: {
+                
+                    nombre: 'Pokemon x AR',
+                    logo: 'https://user-images.githubusercontent.com/61214852/118196015-8391ae00-b409-11eb-9cbc-a40b9bae3aa5.png',
+                    descripcion: `
+                        Es una pagina que cree con los diferentes tipos de realidad aumentada en AR.js con un plus de Model Viewer.
+                        Logre obtener los conocimientos para realizar el proyecto en EDteam en el curso Realida aumentada en la web con AR.js.
+                        Tengo pensado ir modificando o adaptando el sitio web, para explotar mejor los conocimientos.
+                    `,
+                    publictSite: 'https://gustavo-exe.github.io/ArJsPokemon/',
+                    repositorio: 'https://github.com/gustavo-exe/ArJsPokemon',
+                    tools: ['2', '4' , '11', '17']
+                
+            },
+            gallery: [
+                'https://raw.githubusercontent.com/gustavo-exe/Portafolio/c0ff4151bc7d4597166e944ba537205f37890d1f/imgs/MockupPokemon.jpg',
+                'https://raw.githubusercontent.com/gustavo-exe/ArJsPokemon/master/assets/Portadas/ImageTracking.jpg'
+            ]
+        },
+        
+        {
+            id: 3,
             header: {
                 
                     nombre: 'Pokemon x AR',
@@ -39,23 +106,29 @@ export const Proyects = () => {
 
             <div  className="xl:pl-20 xl:pr-20  sm:pr-4 pl-4 pr-4 " >
 
-                {
-                    listProyects.map(proyect=>{
-                        return(
-                            <>
-                                <ProyectHeader 
-                                    key={proyect.id} 
-                                    proyectHeader={proyect.header}  
-                                />
+                <Carousel>
 
-                                <ProyectGallery
-                                    key={proyect.id}
-                                    gallery={proyect.gallery}
-                                />
-                            </>
-                        )
-                    })
-                }
+                {/* <section  className="flex overflow-hidden relative" > */}
+                    
+                    {
+                        listProyects.map(proyect=>{
+                            return(
+                                <div  className={style.theWidth} >
+                                    <ProyectHeader 
+                                        key={proyect.id} 
+                                        proyectHeader={proyect.header}  
+                                    />
+
+                                    <ProyectGallery
+                                        key={proyect.id}
+                                        gallery={proyect.gallery}
+                                    />
+                                </div>
+                            )
+                        })
+                    }
+                {/* </section> */}
+                </Carousel>
                 
             </div>
 
